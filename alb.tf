2ce683fb333b7cb2fc6086eb5a1dec6704dd3135
edeak54 <shane310804@gmail.com>
2026-02-19 02:22:18 +0700

    hopefully final commit lol

---
+++ alb.tf
@@ -0,0 +1,31 @@
+resource "aws_lb" "main" {
+  name               = "project1-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [for s in aws_subnet.public : s.id]
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "app_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web_server.id
+  port             = 80
+}
--- output.tf
+++ output.tf
@@ -10,3 +10,13 @@ output "public_subnet_ids" {
 output "alb_security_group_id" {
   value = aws_security_group.alb_sg.id
 }
+
+output "alb_dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "success_url" {
+  description = "Click this link to see your deployed website"
+  value       = "http://${aws_lb.main.dns_name}"
+}
